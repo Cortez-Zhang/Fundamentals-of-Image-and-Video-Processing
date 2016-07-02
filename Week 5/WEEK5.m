0f4ed77f9ecd9f60d34ea0cfe7714b6bd0412d73
Cortez-Zhang <zhangzhujun36@gmail.com>
2016-07-02 13:58:51 +0800

    PSNR

--- Week 5/WEEK5.m
+++ Week 5/WEEK5.m
@@ -15,11 +15,19 @@ I4=medfilt2(I3);
 figure;
 imshow(I4);
 %psnr
+%import
+IO=imread('original.jpg');
+IO2=im2double(IO);
 %calculate the MSE;
 [a,b]=size(I);
-MSE=1/(a*b)*sum(sum((I-I2).^2));
-MSE2=1/(a*b)*sum(sum((I-I3).^2));
+MSE=1/(a*b)*sum(sum((IO2-I2).^2));
+
 %PSNR
 MAX=1;
 PSNR=10*log10(MAX^2/MSE);
+% one-pass
+MSE1=1/(a*b)*sum(sum((IO2-I3).^2));
+PSNR1=10*log10(MAX^2/MSE1);
+% second-pass
+MSE2=1/(a*b)*sum(sum((IO2-I4).^2));
 PSNR2=10*log10(MAX^2/MSE2);
